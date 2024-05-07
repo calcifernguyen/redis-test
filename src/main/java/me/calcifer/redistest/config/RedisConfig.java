4a74c63f8cca663ddcf5df918b0956becce0aec3
Calcifer Nguyen <calcifer.nth@gmail.com>
2024-05-08 03:47:59 +0700

    RedisCacheService

--- src/main/java/me/calcifer/redistest/config/RedisConfig.java
+++ src/main/java/me/calcifer/redistest/config/RedisConfig.java
@@ -1,74 +1,66 @@
 package me.calcifer.redistest.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
-//    @Value("${spring.data.redis.host}")
-//    private String redisHost;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int redisPort;
-//
-//    @Value("${spring.data.redis.password:#{null}}")
-//    private String redisPassword;
+//    @Bean
+//    RedisConnectionFactory lettuceConnectionFactory() {
+//        return new LettuceConnectionFactory();
+//    }
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        return RedisCacheManager.create(connectionFactory);
+    @Primary
+    RedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
     }
 
-//    @Bean(name = "redisTemplateObject")
-//    public RedisTemplate<String, Object> redisTemplateObject() {
-//        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Object.class));
-//        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
-//        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-//        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-//        return redisTemplate;
-//    }
-//
-//    @Bean(name = "redisTemplateJson")
-//    public RedisTemplate<String, Object> redisTemplateWithJsonSerializer() {
-//        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Object.class));
-//        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-//        return redisTemplate;
-//    }
+    @Bean
+    RedisTemplate<String, Object> jdkRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(RedisSerializer.java());
+        template.setValueSerializer(RedisSerializer.java());
+        template.setHashKeySerializer(RedisSerializer.java());
+        template.setHashValueSerializer(RedisSerializer.java());
+        return template;
+    }
 
-//    @Bean
-//    @Primary
-//    public RedisConnectionFactory jedisConnectionFactory() {
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost,
-//                redisPort);
-//        if (redisPassword != null) {
-//            redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPassword));
-//        }
-//        return new JedisConnectionFactory(redisStandaloneConfiguration);
-//    }
+    @Bean
+    RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
 
-//    @Bean
-//    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
-//        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-//        template.setConnectionFactory(connectionFactory);
-//        // Additional configuration...
-//        return template;
-//    }
+    @Bean
+    RedisTemplate<String, Object> jsonRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(RedisSerializer.json());
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashKeySerializer(RedisSerializer.json());
+        template.setHashValueSerializer(RedisSerializer.json());
+        return template;
+    }
+
+    @Bean
+    RedisTemplate<String, Object> byteRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(RedisSerializer.byteArray());
+        template.setValueSerializer(RedisSerializer.byteArray());
+        template.setHashKeySerializer(RedisSerializer.byteArray());
+        template.setHashValueSerializer(RedisSerializer.byteArray());
+        return template;
+    }
 }
